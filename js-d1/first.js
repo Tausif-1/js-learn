56e302c359a2f7b35516720b99b3f85fd7dd9cb2
Tausif Alam <iamtausif47@gmail.com>
2022-12-19 17:33:01 +0530

    update the log

--- js-d1/first.js
+++ js-d1/first.js
@@ -1,2 +1,4 @@
 console.log("hello world!");
 console.log("hello tausif!");
+// first program 
+// double slash is use to write a comment and it is not executed when program is run
---
+++ js-d1/var_file2.js
@@ -0,0 +1,55 @@
+// variable :- it is use to store some data or information , we can use that information later and we can change that information later.
+
+"use strict"; // it is use to avoid maisktakes in our program and it give error when we do something mistakes in our program.
+
+// how to declare variable?
+
+// we use [var] to declare a varibale.
+// ex:- code
+
+
+var firstName = "tausif";
+
+
+console.log(firstName);
+
+// change the value
+
+var firstName1 = "tausif";
+firstName1 = "alam";
+console.log(firstName1)
+
+//it change the value of variable and remove tausif and add alam.
+
+
+// rules for naming variable :-----
+
+// 1. you can't start with number.
+// ex:--
+// 1value - [invalid variable]
+// value1 - [valid variable]
+
+// 2. you can use only underscore _ or doller $ symbol to create a variable.
+// ex:--
+// _firstname - [valid variable]
+// first_name - [valid variable]
+// $firstname - [valid variable]
+// first$name - [valid variable]
+
+// 3. you can't use spaces to declare a variable.
+// ex:- 
+// first name - [invalid]
+
+// 4. convention means start with samll letter and use camelcase writing method.
+
+
+// snakecase writing - [first_name] - it start with samll letter and use underscore or doller symbol to make a variable.
+
+// camelcase writing - [firstName] -it start with samll letter and use a capital letter to make variable.
+ 
+// 5. variable names are casesensitive capital A and small a is both different variable names.
+
+
+
+// let keyword is use to make variables .all programmers are using let keyword instead of var key word.
+
